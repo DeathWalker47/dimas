d37d49fb36426c6cac848c21a14670fb9b605640
DeathWalker47 <fizer_00@mail.ru>
2022-08-17 18:48:50 +0300

    почти доделал секцию с проектами

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 // Подключаем созданные файлы в папке components
 import './components/func.js';
 import './components/_tabs';
+import './components/_project';
---
+++ src/js/components/_project.js
@@ -0,0 +1,19 @@
+const listItem = document.querySelectorAll(".project-list__item");
+const project = document.querySelector(".project");
+
+listItem.forEach((el) => {
+  el.addEventListener("mouseover", function (e) {
+    const self = e.currentTarget;
+    listItem.forEach((el) => el.classList.remove("project-list__item--active"));
+    self.classList.add("project-list__item--active");
+    // console.log(self.dataset.src);
+    project.setAttribute(
+      "style",
+      `background-image: url(../img/proj-${self.dataset.src}.jpg)`
+    );
+
+    // console.log(project.getAttribute('style'));
+  });
+});
+
+// project.setAttribute('style', `background-image: url(../img/proj-${self.dataset.src}.jpg)`)
