ae4083d8f849cd96c46609c1ad42ca7955c556f4
DeathWalker47 <fizer_00@mail.ru>
2022-11-23 23:36:53 +0300

    Делаю свайпер

--- src/js/components/_page-slider.js
+++ src/js/components/_page-slider.js
@@ -3,8 +3,37 @@ import Swiper, { Navigation, Pagination, EffectCreative, Mousewheel } from 'swip
 
 Swiper.use([Navigation, Pagination, EffectCreative, Mousewheel]);
 
-const swiper = new Swiper('.swiper-page', {
+
+const swiperHome = new Swiper('.home-swiper', {
+  speed: 1000,
+  // navigation: {
+  //   nextEl: '.swiper-page__next',
+  //   prevEl: '.swiper-page__prev',
+  // },
+  mousewheel: {
+    invert: false,
+  },
   // slidesPerView: 1,
+  // direction: "vertical",
+  pagination: {
+    el: '.home-swiper__pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  // grabCursor: true,
+  //   effect: "creative",
+  //   creativeEffect: {
+  //     prev: {
+  //       // shadow: true,
+  //       translate: ["-20%", 0, -1],
+  //     },
+  //     next: {
+  //       translate: ["100%", 0, 0],
+  //     },
+  //   },
+});
+
+const swiper = new Swiper('.swiper-page', {
   speed: 1000,
   navigation: {
     nextEl: '.swiper-page__next',
@@ -16,6 +45,7 @@ const swiper = new Swiper('.swiper-page', {
   pagination: {
     el: '.swiper-page__pagination',
     type: 'bullets',
+    clickable: true,
   },
   grabCursor: true,
     effect: "creative",
