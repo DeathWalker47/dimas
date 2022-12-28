9e14cba4d491e17413821a7c1a4473cd7b99115b
DeathWalker47 <fizer_00@mail.ru>
2022-12-28 23:41:16 +0300

    Закончил адапвтив главное

--- src/js/components/_active-menu.js
+++ src/js/components/_active-menu.js
@@ -1,16 +1,34 @@
-const main = document.querySelector('.main')
+const main = document.querySelector(".main");
 
-if(main.dataset) {
-const navSliderListLink = document.querySelectorAll('.nav__link');
-  navSliderListLink.forEach(el => {
-    if(el.dataset.text === main.dataset.descr) {
-      el.classList.add('nav__link--active')
+if (main.dataset) {
+  const navSliderListLink = document.querySelectorAll(".nav__link");
+  navSliderListLink.forEach((el) => {
+    if (el.dataset.text === main.dataset.descr) {
+      el.classList.add("nav__link--active");
     }
-  })
+  });
 }
 
-const burder = document.querySelector('.burger');
+const burder = document.querySelector(".burger");
+const mobileNav = document.querySelector(".nav");
+const navLink = document.querySelectorAll(".nav__link");
 
-burder.addEventListener('click', ()=> {
-  burder.classList.toggle('burger--active');
-})
+burder.addEventListener("click", () => {
+  burder.classList.toggle("burger--active");
+  mobileNav.classList.toggle("nav--active");
+});
+
+// navLink.addEventListener('click', ()=> {
+
+// })
+navLink.forEach((el) => {
+  el.addEventListener("click", () => {
+    if (
+      mobileNav.classList.contains("nav--active") &&
+      burder.classList.contains("burger--active")
+    ) {
+      mobileNav.classList.remove("nav--active");
+      burder.classList.remove("burger--active");
+    }
+  });
+});
